ca612ef4aff4ee02b0db64845eebdbe990dd3d1a
liam-patel1 <liam.patel@city.ac.uk>
2025-04-05 23:24:42 +0100

    test

--- main.cpp
+++ main.cpp
@@ -14,15 +14,12 @@ int main() {
 
 	auto image1 = loadPNG("C:\\Users\\liamp\\Desktop\\untitled.png");
 	auto image2 = loadPNG("C:\\Users\\liamp\\Desktop\\untitled2.png");
-	auto image3 = loadPNG("C:\\Users\\liamp\\Desktop\\untitled3.png");
 
 	CHECK(image1);
 	CHECK(image2);
-	CHECK(image3);
 
 	new lpcv::Viewer(image1.value());
 	new lpcv::Viewer(image2.value());
-	new lpcv::Viewer(image3.value());
 
 	a.exec();
 	return 0;
